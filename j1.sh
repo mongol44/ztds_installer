3b4e661cb9e744cc226dd0157175421047f9cccd
SPART-PC\spart <spartanets.ru@gmail.com>
2018-08-30 00:13:01 +0300

    no message

--- j1.sh
+++ j1.sh
@@ -20,6 +20,9 @@ mkdir -m 777 /var/lib/php/session
 
 mkdir -m 777 /var/www/html/$domain
 
+rm -rf /tmp/ztds
+rm -f /tmp/ztds.7z
+
 curl -L -o /tmp/ztds.7z https://github.com/spartanetsru/ztds_installer/blob/master/$ztds_version.7z?raw=true
 
 7za x -o/tmp/ztds /tmp/ztds.7z
