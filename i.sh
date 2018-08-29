d35296d04d13c86577fdaee143a43c072078a0ea
SPART-PC\spart <spartanets.ru@gmail.com>
2018-08-29 23:54:55 +0300

    no message

--- i.sh
+++ i.sh
@@ -23,7 +23,7 @@ curl -L -o /tmp/ztds.7z https://github.com/spartanetsru/ztds_installer/blob/mast
 
 7za x -o/tmp/ztds /tmp/ztds.7z
 
-cp -rf /tmp/ztds/$ztds_version /var/www/html/$domain
+cp -rf /tmp/ztds/$ztds_version/. /var/www/html/$domain
 
 chmod 777 -R /var/www/html/$domain
 chown -R nginx:nginx /var/www/html/$domain
