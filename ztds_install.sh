4631e429614d11b6630fae0b554aaa6d808f2a9a
SPART-PC\spart <spartanets.ru@gmail.com>
2018-08-29 23:17:27 +0300

    no message

--- ztds_install.sh
+++ ztds_install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-read -p "Run command $foo? [yn]" answer
+read -p "Run command $foo? [yn]" answer </dev/tty
 
 firewall-cmd --permanent --zone=public --add-service=http
 firewall-cmd --permanent --zone=public --add-service=https
 firewall-cmd --reload
 
 echo -n "Enter domain name (e.g. site.ru) and press [ENTER]: "
-read -p domain
+read -p domain </dev/tty
 
 yum install -y epel-release nginx php-fpm php-cli php-gd php-ldap php-odbc php-pdo php-pecl-memcache php-pear php-xml php-xmlrpc php-mbstring php-snmp php-soap
 
@@ -21,7 +21,6 @@ mkdir -m 777 /var/lib/php/session
 mkdir -m 777 /var/www/html/$domain
 
 echo -n 'Please upload files to the /var/www/html/$domain and press [ENTER]:';
-
-read -p done
+read -p done </dev/tty
 
 chmod 777 -R /var/www/html/$domain
