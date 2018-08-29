55ff24e81f782436b82ee7806de6816a59be7c76
SPART-PC\spart <spartanets.ru@gmail.com>
2018-08-30 00:54:11 +0300

    no message

--- a.sh
+++ a.sh
@@ -5,7 +5,7 @@ ztds_version='ztds_v0.7.3'
 clear
 
 password=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
-password_md5=var=$password|md5sum
+password_md5=$password|md5sum
 
 api_key=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 6 | head -n 1)
 postback_key=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 6 | head -n 1)
